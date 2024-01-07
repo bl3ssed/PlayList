0dd674e36fe560b5dd2dd4a0b24babef98001213
Gdhdh Likry <ralnikov08@gmail.com>
2024-01-07 14:18:22 +0300

    itog

---
+++ Controllers/FAQController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlayList.Models;
+
+namespace PlayList.Controllers
+{
+    public class FAQController : Controller
+    {
+        public IActionResult Index() //faq
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Check(FAQ faq) //faq
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/FAQ.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlayList.Models
+{
+    public class FAQ
+    {
+        [Display(Name ="Введите Имя")]
+        [Required(ErrorMessage ="Введите верное значение")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите Возраст")]
+        [Required(ErrorMessage = "Введите верное значение")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите ваш отзыв")]
+        [Required(ErrorMessage = "Введите верное значение")]
+        public string Reply { get; set; }
+    }
+}
