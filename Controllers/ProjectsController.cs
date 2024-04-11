96523d8d78cc4c5f08a1b5325f322e4229864fcf
blessed <blessed@DESKTOP-F4R41FB>
2024-04-11 22:57:02 +0300

    main

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PlayList.Models;
+using System.Diagnostics;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class ProjectsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
